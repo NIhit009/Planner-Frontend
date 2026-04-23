6cedb399af20830da6acd028a5dc3061e871366a
NIhit009 <your-correct-email@example.com>
2026-04-23 08:36:50 +0545

    auth fixes 2

--- lib/Base_url.ts
+++ lib/Base_url.ts
@@ -1,6 +1,6 @@
 export const BASE_URL = process.env.NODE_ENV !== 'production' 
   ? process.env.NEXT_PUBLIC_LOCAL_API_URL 
-  : process.env.NEXT_PUBLIC_PRODUCTION_API_URL;
+  : process.env.NEXT_PUBLIC_API_URL;
 
 console.log("Current Environment:", process.env.NODE_ENV);
 console.log("Resolved Base URL:", BASE_URL);
